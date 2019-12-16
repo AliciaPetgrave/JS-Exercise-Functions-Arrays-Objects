8ce029f17d160ad64c1bb8e7a926c023a80fc4b7
Alicia Petgrave <alicia.petgrave@gmail.com>
2019-12-16 15:52:53 -0500

    getName passing

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ function makePersonObject(identry, nameentry, emailentry){
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(person) {
+  return `Hello, my name is ${person.name}`;
 }
 
 /**
